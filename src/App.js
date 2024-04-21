81071fa12246d78288e2f43e5c32eb8ffedc7b33
qulmuhammedov <161331505+qulmuhammedov0707@users.noreply.github.com>
2024-04-21 15:41:55 +0500

    Routeradd main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Login from './pages/Login';
+import RouterComponent from './routes';
 
 function App() {
   return (
     <>
-    <Login/>
+    <RouterComponent/>
     </>
   );
 }
